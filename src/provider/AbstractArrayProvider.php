40dc4f2a19d6df351dd7008e177b72e252db3d7b
Brent Roose <brent.roose@gmail.com>
2016-10-11 07:52:17 +0200

    Refactor remove parseSingle

---
+++ src/provider/AbstractArrayProvider.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace brendt\stitcher\provider;
+
+use brendt\stitcher\factory\ProviderFactory;
+
+abstract class AbstractArrayProvider extends AbstractProvider {
+
+    /**
+     * @var ProviderFactory
+     */
+    protected $providerFactory;
+
+    /**
+     * AbstractProvider constructor.
+     *
+     * @param $root
+     */
+    public function __construct($root) {
+        parent::__construct($root);
+
+        $this->providerFactory = new ProviderFactory($root);
+    }
+
+    /**
+     * @param array $data
+     * @param null  $id
+     *
+     * @return mixed
+     */
+    protected function parseArrayData(array $data, $id = null) {
+        $result = [];
+
+        if (isset($data['entries'])) {
+            foreach ($data['entries'] as $id => $entry) {
+                $result[$id] = $this->parseArrayData($entry, $id);
+            }
+
+            return $result;
+        }
+
+        foreach ($data as $field => $value) {
+            if (is_array($value) && isset($value['type']) && isset($value['path'])) {
+                $provider = $this->providerFactory->getByType($value['type']);
+
+                if (!$provider) {
+                    continue;
+                }
+
+                $result[$field] = $provider->parse($value['path']);
+            } else {
+                $result[$field] = $value;
+            }
+        }
+
+        if (!isset($result['id'])) {
+            $result['id'] = $id;
+        }
+
+        return $result;
+    }
+
+}
