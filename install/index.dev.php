9798a23eee9936c1857865cc118dc8a385f484df
BrenDt <brent@statik.be>
2016-12-10 15:46:00 +0100

    Add DevController tests

--- install/index.dev.php
+++ install/index.dev.php
@@ -7,4 +7,4 @@ use brendt\stitcher\controller\DevController;
 // This controller will render HTML pages on the fly.
 // See config.dev.yml for more information.
 $controller = new DevController();
-$controller->run();
+echo $controller->run();
