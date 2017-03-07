63d0b72c010b5cb8a57dd49c32b5e803fcbeb5cb
BrenDt <brent@statik.be>
2017-03-07 13:12:38 +0100

    Refactor ParserFactory to clean up code

--- src/Factory/ParserFactory.php
+++ src/Factory/ParserFactory.php
@@ -2,27 +2,30 @@
 
 namespace Brendt\Stitcher\Factory;
 
+use Brendt\Stitcher\Exception\ParserException;
 use Brendt\Stitcher\Parser\Parser;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ParserFactory
 {
     const EXTENSION_JSON = 'json';
-    const EXTENSION_MD = 'md';
-    const EXTENSION_FOLDER = '/';
-    const EXTENSION_YML = 'yml';
+    const EXTENSION_MD = 'markdown';
+    const EXTENSION_FOLDER = 'folder';
+    const EXTENSION_YML = 'yaml';
     const EXTENSION_YAML = 'yaml';
-    const EXTENSION_IMG = 'img';
-    const EXTENSION_CSS = 'css';
-    const EXTENSION_JS = 'js';
+    const EXTENSION_IMG = 'image';
+    const EXTENSION_JS = 'file';
+    const EXTENSION_CSS = 'file';
     const EXTENSION_SASS = 'sass';
-    const EXTENSION_SCSS = 'scss';
+    const EXTENSION_SCSS = 'sass';
     const PARSER_DEFAULT = 'default';
 
     /**
      * @var ContainerInterface
      */
     private $container;
+
+    private static $typeFilters = [];
 
     /**
      * ParserFactory constructor.
@@ -31,6 +34,46 @@ class ParserFactory
      */
     public function __construct(ContainerInterface $container) {
         $this->container = $container;
+
+        self::addTypeFilter(self::EXTENSION_FOLDER, function ($fileName) {
+            return strpos($fileName, '/') === strlen($fileName) - 1;
+        });
+
+        self::addTypeFilter(self::EXTENSION_JSON, function ($fileName) {
+            return strpos($fileName, '.json') === strlen($fileName) - 5;
+        });
+
+        self::addTypeFilter(self::EXTENSION_MD, function ($fileName) {
+            return strpos($fileName, '.md') !== false;
+        });
+
+        self::addTypeFilter(self::EXTENSION_YML, function ($fileName) {
+            return strpos($fileName, '.yaml') !== false || strpos($fileName, '.yml') !== false;
+        });
+
+        self::addTypeFilter(self::EXTENSION_IMG, function ($fileName) {
+            return strpos($fileName, '.jpg') !== false || strpos($fileName, '.png') !== false;
+        });
+
+        self::addTypeFilter(self::EXTENSION_CSS, function ($fileName) {
+            return strpos($fileName, '.css') !== false;
+        });
+
+        self::addTypeFilter(self::EXTENSION_JS, function ($fileName) {
+            return strpos($fileName, '.js') === strlen($fileName) - 3;
+        });
+
+        self::addTypeFilter(self::EXTENSION_SASS, function ($fileName) {
+            return strpos($fileName, '.scss') !== false || strpos($fileName, '.sass') !== false;
+        });
+    }
+
+    /**
+     * @param string   $type
+     * @param callable $filter
+     */
+    public static function addTypeFilter(string $type, callable $filter) {
+        self::$typeFilters[$type][] = $filter;
     }
 
     /**
@@ -43,73 +86,34 @@ class ParserFactory
             return null;
         }
 
-        if (strpos($fileName, '/') === strlen($fileName) - 1) {
-            return $this->getByType(self::EXTENSION_FOLDER);
+        $type = self::PARSER_DEFAULT;
+
+        foreach (self::$typeFilters as $filterType => $filters) {
+            foreach ($filters as $filterCheck) {
+                if ($filterCheck($fileName)) {
+                    $type = $filterType;
+                    break;
+                }
+            }
         }
 
-        if (strpos($fileName, '.json') !== false) {
-            return $this->getByType(self::EXTENSION_JSON);
-        }
-
-        if (strpos($fileName, '.md') !== false) {
-            return $this->getByType(self::EXTENSION_MD);
-        }
-
-        if (strpos($fileName, '.yml') !== false) {
-            return $this->getByType(self::EXTENSION_YML);
-        }
-
-        if (strpos($fileName, '.jpg') !== false) {
-            return $this->getByType(self::EXTENSION_IMG);
-        }
-
-        if (strpos($fileName, '.png') !== false) {
-            return $this->getByType(self::EXTENSION_IMG);
-        }
-
-        if (strpos($fileName, '.css') !== false) {
-            return $this->getByType(self::EXTENSION_CSS);
-        }
-
-        if (strpos($fileName, '.js') !== false) {
-            return $this->getByType(self::EXTENSION_JS);
-        }
-
-        if (strpos($fileName, '.scss') !== false || strpos($fileName, '.sass') !== false) {
-            return $this->getByType(self::EXTENSION_SASS);
-        }
-
-        return $this->getByType(self::PARSER_DEFAULT);
+        return $this->getByType($type);
     }
 
     /**
      * @param string $type
      *
-     * @return mixed
+     * @return Parser|mixed
+     * @throws ParserException
      */
     public function getByType($type) : Parser {
-        switch ($type) {
-            case self::EXTENSION_IMG:
-                return $this->container->get('parser.image');
-            case self::EXTENSION_FOLDER:
-                return $this->container->get('parser.folder');
-            case self::EXTENSION_MD:
-                return $this->container->get('parser.markdown');
-            case self::EXTENSION_JSON:
-                return $this->container->get('parser.json');
-            case self::EXTENSION_YML:
-            case self::EXTENSION_YAML:
-                return $this->container->get('parser.yaml');
-            case self::EXTENSION_JS:
-            case self::EXTENSION_CSS:
-                return $this->container->get('parser.file');
-            case self::EXTENSION_SCSS:
-            case self::EXTENSION_SASS:
-                return $this->container->get('parser.sass');
-            case self::PARSER_DEFAULT:
-            default:
-                return $this->container->get('parser.default');
+        $key = "parser.{$type}";
+
+        if (!$this->container->has($key)) {
+            throw new ParserException("Parser with the key {$key} not found as a service.");
         }
+
+        return $this->container->get($key);
     }
 
 }
