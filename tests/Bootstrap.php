c16b15a94605f834fbbf719023e910584846bf37
BrenDt <brent@stitcher.io>
2017-04-16 15:44:05 +0200

    Update namespacing

---
+++ tests/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Filesystem\Filesystem;
+
+register_shutdown_function(function () {
+    $fs = new Filesystem();
+
+    if ($fs->exists(__DIR__ . '/.cache')) {
+        $fs->remove(__DIR__ . '/.cache');
+    }
+
+    if ($fs->exists(__DIR__ . '/public')) {
+        $fs->remove(__DIR__ . '/public');
+    }
+});
--- tests/Brendt/Stitcher/Factory/HeaderCompilerFactoryTest.php
+++ tests/Brendt/Stitcher/Factory/HeaderCompilerFactoryTest.php
@@ -1,22 +1,22 @@
 <?php
-
-namespace Brendt\Stitcher\Factory;
-
-use Brendt\Stitcher\Stitcher;
-
-class HeaderCompilerFactoryTest extends \PHPUnit_Framework_TestCase
-{
-    public function setUp() {
-        Stitcher::create('./tests/config.yml');
-    }
-
-    /**
-     * @test
-     */
-    public function it_finds_the_runtime_compiler() {
-        /** @var HeaderCompilerFactory $factory */
-        $factory = Stitcher::get('factory.header.compiler');
-
-        $compiler = $factory->setEnvironment('development');
-    }
-}
+//
+//namespace Brendt\Stitcher\Factory;
+//
+//use Brendt\Stitcher\Stitcher;
+//
+//class HeaderCompilerFactoryTest extends \PHPUnit_Framework_TestCase
+//{
+//    public function setUp() {
+//        Stitcher::create('./tests/config.yml');
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function it_finds_the_runtime_compiler() {
+//        /** @var HeaderCompilerFactory $factory */
+//        $factory = Stitcher::get('factory.header.compiler');
+//
+//        $compiler = $factory->setEnvironment('development');
+//    }
+//}
