cd80421582b0a3bf96cac75aa6b3449068782f7b
BrenDt <brent@stitcher.io>
2017-04-25 17:26:23 +0200

    DevController refactor

--- src/Brendt/Stitcher/Application/DevController.php
+++ src/Brendt/Stitcher/Application/DevController.php
@@ -2,7 +2,6 @@
 
 namespace Brendt\Stitcher\Application;
 
-use Brendt\Stitcher\App;
 use Brendt\Stitcher\Exception\StitcherException;
 use Brendt\Stitcher\Factory\AdapterFactory;
 use Brendt\Stitcher\Stitcher;
@@ -27,14 +26,10 @@ class DevController
     /**
      * Create a new developer controller with optional configuration path and -file.
      *
-     * @param string $path
-     * @param string $name
+     * @param Stitcher $stitcher
      */
-    public function __construct($path = './', $name = 'config.dev.yml') {
-        $path = rtrim($path, '/');
-        $name = ltrim($name, '/');
-
-        $this->stitcher = App::init("{$path}/{$name}")::get('stitcher');
+    public function __construct(Stitcher $stitcher) {
+        $this->stitcher = $stitcher;
     }
 
     /**
@@ -51,12 +46,14 @@ class DevController
         }
 
         try {
-            return $this->getPage($url);
+            echo $this->getPage($url);
         } catch (StitcherException $e) {
-            return $e->getMessage();
+            echo $e->getMessage();
         } catch (ResourceNotFoundException $e) {
-            return "404";
+            echo "404";
         }
+
+        return;
     }
 
     /**
