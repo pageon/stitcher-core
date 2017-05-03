56ce31ee223b00ce66593d9f0bad70be230c073a
BrenDt <brent@stitcher.io>
2017-05-03 17:06:13 +0200

    Add `init` to Plugin

--- src/Brendt/Stitcher/Plugin/Plugin.php
+++ src/Brendt/Stitcher/Plugin/Plugin.php
@@ -4,6 +4,12 @@ namespace Brendt\Stitcher\Plugin;
 
 interface Plugin
 {
+    /**
+     * Initialise your plugin.
+     *
+     * @return void
+     */
+    public function init();
 
     /**
      * Get the location of your plugin's `config.yml` file.
