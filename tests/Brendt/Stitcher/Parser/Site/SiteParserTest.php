ed9f07c14ca25d4cee6f612634a2bd45ed6127b0
BrenDt <brent@stitcher.io>
2017-05-13 15:03:39 +0200

    Added `Brendt\Stitcher\Parser\Site\PageParser` to parse a single page, which is no longer the responsibility of `SiteParser`

---
+++ tests/Brendt/Stitcher/Parser/Site/SiteParserTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Brendt\Stitcher\Parser\Site;
+
+use Brendt\Stitcher\App;
+use Brendt\Stitcher\Site\Http\Header;
+use Brendt\Stitcher\Site\Http\Htaccess;
+use Brendt\Stitcher\Site\Page;
+use PHPUnit\Framework\TestCase;
+
+class SiteParserTest extends TestCase
+{
+    protected function setUp() {
+        App::init('./tests/config.yml');
+    }
+
+    private function createSiteParser() : SiteParser {
+        /** @var SiteParser $parser */
+        $parser = App::get('parser.site');
+
+        return $parser;
+    }
+
+    public function test_site_loading() {
+        $siteParser = $this->createSiteParser();
+        $site = $siteParser->loadSite();
+
+        foreach ($site as $page) {
+            $this->assertNotNull($page->getId());
+        }
+    }
+}
