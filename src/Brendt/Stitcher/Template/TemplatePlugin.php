bfeed6cec22c1b73e24024aa20d46c829dca87ae
BrenDt <brent@statik.be>
2017-05-18 06:59:03 +0200

    Bugfix for general meta configuration overriding other meta values.

--- src/Brendt/Stitcher/Template/TemplatePlugin.php
+++ src/Brendt/Stitcher/Template/TemplatePlugin.php
@@ -54,19 +54,13 @@ class TemplatePlugin
      */
     private $page;
 
-    /**
-     * @var array
-     */
-    private $meta;
-
     public function __construct(
         ParserFactory $parserFactory,
         ResponsiveFactory $responsiveFactory,
         CSSmin $cssMinifier,
         string $publicDir,
         string $srcDir,
-        bool $minify,
-        $meta
+        bool $minify
     ) {
         $this->parserFactory = $parserFactory;
         $this->responsiveFactory = $responsiveFactory;
@@ -74,7 +68,6 @@ class TemplatePlugin
         $this->publicDir = $publicDir;
         $this->srcDir = $srcDir;
         $this->minify = $minify;
-        $this->meta = (array) $meta;
     }
 
     /**
@@ -97,11 +90,6 @@ class TemplatePlugin
      */
     public function meta(array $extra = []) : string {
         $meta = $this->page ? $this->page->meta : new Meta();
-
-        foreach ($this->meta as $name => $content) {
-            $meta->name($name, $content);
-        }
-
         foreach ($extra as $name => $content) {
             $meta->name($name, $content);
         }
