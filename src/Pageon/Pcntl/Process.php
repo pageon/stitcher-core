64bdfb78da5ebc0f3d77985d3db3b19bba50f05f
BrenDt <brent@statik.be>
2017-06-07 09:05:56 +0200

    Async support

--- src/Pageon/Pcntl/Process.php
+++ src/Pageon/Pcntl/Process.php
@@ -4,15 +4,83 @@ namespace Pageon\Pcntl;
 
 abstract class Process
 {
+    protected $pid;
+
     protected $name;
 
-    public function __construct(string $name) {
+    protected $socket;
+
+    protected $success;
+
+    protected $startTime;
+
+    protected $maxRunTime = 300;
+
+    public abstract function execute();
+
+    public function setPid($pid) : Process {
+        $this->pid = $pid;
+
+        return $this;
+    }
+
+    public function getPid() {
+        return $this->pid;
+    }
+
+    public function setSocket($socket) : Process {
+        $this->socket = $socket;
+
+        return $this;
+    }
+
+    public function getSocket() {
+        return $this->socket;
+    }
+
+    public function setName(string $name) : Process {
         $this->name = $name;
+
+        return $this;
     }
 
     public function getName() : string {
         return $this->name;
     }
 
-    public abstract function execute();
+    public function onSuccess(callable $callback) {
+        $this->success = $callback;
+    }
+
+    public function getSuccess() : ?callable {
+        return $this->success;
+    }
+
+    public function setStartTime($startTime) {
+        $this->startTime = $startTime;
+
+        return $this;
+    }
+
+    public function getStartTime() {
+        return $this->startTime;
+    }
+
+    public function setMaxRunTime(int $maxRunTime) : Process {
+        $this->maxRunTime = $maxRunTime;
+
+        return $this;
+    }
+
+    public function getMaxRunTime() : int {
+        return $this->maxRunTime;
+    }
+
+    public function triggerSuccess() {
+        if (!$this->success) {
+            return null;
+        }
+
+        return call_user_func_array($this->success, [$this]);
+    }
 }
