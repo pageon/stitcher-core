180be43fc0883ebd99b78c911323f4e3042dd757
BrenDt <brent@statik.be>
2017-06-11 22:13:32 +0200

    Code cleanup

--- src/Pageon/Pcntl/Manager.php
+++ src/Pageon/Pcntl/Manager.php
@@ -2,28 +2,24 @@
 
 namespace Pageon\Pcntl;
 
-use Brendt\Stitcher\Event\Event;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
+/**
+ * This Manager is used to create and wait for forked processes which can be executed in parallel.
+ * It's a wrapper around `ext-pcntl`.
+ *
+ * Class Manager
+ * @package Pageon\Pcntl
+ */
 class Manager
 {
     /**
-     * @var EventDispatcher
+     * Create an asynchronous process.
+     *
+     * @param Process $process The process to run asynchronous.
+     *
+     * @return Process The asynchronous process.
      */
-    private $eventDispatcher;
-
-    private $children;
-
-    public function __construct(EventDispatcher $eventDispatcher) {
-        pcntl_signal_dispatch();
-        $this->eventDispatcher = $eventDispatcher;
-        $this->children = [];
-
-        pcntl_signal(SIGCHLD, function () {
-
-        });
-    }
-
     public function async(Process $process) : Process {
         socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $sockets);
 
@@ -48,11 +44,21 @@ class Manager
         return $process;
     }
 
+    /**
+     * Wait for a collection of processes to finish.
+     *
+     * @param ProcessCollection $processCollection
+     *
+     * @return array
+     * @throws \Exception
+     */
     public function wait(ProcessCollection $processCollection) {
         $output = [];
         $processes = $processCollection->toArray();
 
-        while (count($processes)) {
+        do {
+            usleep(100000);
+
             /** @var Process $process */
             foreach ($processes as $key => $process) {
                 $processStatus = pcntl_waitpid($process->getPid(), $status, WNOHANG | WUNTRACED);
@@ -75,15 +81,16 @@ class Manager
                         throw new \Exception("Could not reliably manage {$process->getPid()}");
                 }
             }
-
-            if (count($processes)) {
-                usleep(100000);
-            }
-        }
+        } while (count($processes));
 
         return $output;
     }
 
+    /**
+     * Handle a successful process.
+     *
+     * @param Process $process
+     */
     private function handleProcessSuccess(Process $process) {
         $output[] = unserialize(socket_read($process->getSocket(), 4096));
         socket_close($process->getSocket());
@@ -94,6 +101,13 @@ class Manager
         }
     }
 
+    /**
+     * Handle a stopped process.
+     *
+     * @param Process $process
+     *
+     * @throws \Exception
+     */
     private function handleProcessStop(Process $process) {
         if (!posix_kill($process->getPid(), SIGKILL)) {
             throw new \Exception('Failed to kill ' . $process->getPid() . ': ' . posix_strerror(posix_get_last_error()));
