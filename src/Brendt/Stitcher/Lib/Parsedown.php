3bda6234217f52baedddba389e301c3bbdd94974
BrenDt <brent@stitcher.io>
2017-06-11 11:58:18 +0200

    Add Parsedown extension to support `target="_blank"` links by prefixing the URL with `*`.

--- src/Brendt/Stitcher/Lib/Parsedown.php
+++ src/Brendt/Stitcher/Lib/Parsedown.php
@@ -16,9 +16,21 @@ class Parsedown extends LibParsedown
 
         return $block;
     }
-    
-    protected function inlineLink($Excerpt) {
-        $block = parent::inlineLink($Excerpt);
-        return $block;
+
+    protected function element(array $Element) {
+        $markup = parent::element($Element);
+
+        if (!isset($Element['attributes']['href'])) {
+            return $markup;
+        }
+
+        $href = $Element['attributes']['href'];
+        
+        if (strpos($href, '*') !== 0) {
+            return $markup;
+        }
+
+        $href = substr($href, 1);
+        return "<a href='$href' target='_blank' rel='noreferrer noopener'>{$Element['text']}</a>";
     }
 }
