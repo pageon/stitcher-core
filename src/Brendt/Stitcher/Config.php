4212d6364e8a0434b75d74d8f8e425bdf7fa7d1f
BrenDt <brent@statik.be>
2017-08-27 13:20:43 +0200

    Move config defaults to the Config class

--- src/Brendt/Stitcher/Config.php
+++ src/Brendt/Stitcher/Config.php
@@ -13,7 +13,51 @@ use Symfony\Component\Yaml\Yaml;
  */
 class Config
 {
-    public static function parseImports(array $config) : array {
+    const ASYNC = 'async';
+    const ENVIRONMENT = 'environment';
+    const DIRECTORIES_SRC = 'directories.src';
+    const DIRECTORIES_PUBLIC = 'directories.public';
+    const DIRECTORIES_CACHE = 'directories.cache';
+    const DIRECTORIES_HTACCESS = 'directories.htaccess';
+    const META = 'meta';
+    const MINIFY = 'minify';
+    const ENGINES_TEMPLATE = 'engines.template';
+    const ENGINES_IMAGE = 'engines.image';
+    const ENGINES_OPTIMIZER = 'engines.optimizer';
+    const ENGINES_ASYNC = 'engines.async';
+    const CDN = 'cdn';
+    const CACHES_IMAGE = 'caches.image';
+    const CACHES_CDN = 'caches.cdn';
+    const REDIRECT_WWW = 'redirect.www';
+    const REDIRECT_HTTPS = 'redirect.https';
+    const OPTIMIZER_OPTIONS = 'optimizer.options';
+    const SITEMAP_URL = 'sitemap.url';
+
+    public static function getDefaults(): array {
+        return [
+            self::ASYNC                => true,
+            self::ENVIRONMENT          => 'development',
+            self::DIRECTORIES_SRC      => './src',
+            self::DIRECTORIES_PUBLIC   => './public',
+            self::DIRECTORIES_CACHE    => './.cache',
+            self::DIRECTORIES_HTACCESS => './public/.htaccess',
+            self::META                 => [],
+            self::MINIFY               => false,
+            self::ENGINES_TEMPLATE     => 'smarty',
+            self::ENGINES_IMAGE        => 'gd',
+            self::ENGINES_OPTIMIZER    => true,
+            self::ENGINES_ASYNC        => true,
+            self::CDN                  => [],
+            self::CACHES_IMAGE         => true,
+            self::CACHES_CDN           => true,
+            self::REDIRECT_WWW         => false,
+            self::REDIRECT_HTTPS       => false,
+            self::OPTIMIZER_OPTIONS    => [],
+            self::SITEMAP_URL          => null,
+        ];
+    }
+
+    public static function parseImports(array $config): array {
         if (!isset($config['imports'])) {
             return $config;
         }
@@ -42,7 +86,7 @@ class Config
         return $configFiles->current();
     }
 
-    public static function flatten(array $config, string $prefix = '') : array {
+    public static function flatten(array $config, string $prefix = ''): array {
         $result = [];
 
         foreach ($config as $key => $value) {
