394cc0d7b5e730c1fd71db8aa83e14849aefe19d
BrenDt <brent@statik.be>
2017-08-29 08:16:31 +0200

    Add Browser support for PageParser

--- src/Brendt/Stitcher/Parser/Site/PageParser.php
+++ src/Brendt/Stitcher/Parser/Site/PageParser.php
@@ -7,6 +7,7 @@ use Brendt\Stitcher\Factory\AdapterFactory;
 use Brendt\Stitcher\Factory\HeaderCompilerFactory;
 use Brendt\Stitcher\Factory\ParserFactory;
 use Brendt\Stitcher\Factory\TemplateEngineFactory;
+use Brendt\Stitcher\Lib\Browser;
 use Brendt\Stitcher\Site\Http\HeaderCompiler;
 use Brendt\Stitcher\Site\Meta\MetaCompiler;
 use Brendt\Stitcher\Site\Page;
@@ -16,15 +17,15 @@ use Symfony\Component\Finder\SplFileInfo;
 
 class PageParser
 {
+    private $browser;
     private $parserFactory;
     private $metaCompiler;
     private $templatePlugin;
-    private $templateDir;
     private $adapterFactory;
     private $headerCompiler;
 
     public function __construct(
-        string $templateDir,
+        Browser $browser,
         AdapterFactory $adapterFactory,
         ParserFactory $parserFactory,
         HeaderCompilerFactory $headerCompilerFactory,
@@ -32,8 +33,7 @@ class PageParser
         TemplatePlugin $templatePlugin,
         MetaCompiler $metaCompiler
     ) {
-        $this->templateDir = $templateDir;
-
+        $this->browser = $browser;
         $this->adapterFactory = $adapterFactory;
         $this->parserFactory = $parserFactory;
         $this->metaCompiler = $metaCompiler;
@@ -55,7 +55,7 @@ class PageParser
         $templateExtensionsRegex = '/\.(' . implode('|', $templateExtensions) . ')/';
 
         /** @var SplFileInfo[] $files */
-        $files = Finder::create()->files()->in($this->templateDir)->name($templateExtensionsRegex);
+        $files = $this->browser->template()->name($templateExtensionsRegex)->files();
         $templates = [];
 
         foreach ($files as $file) {
