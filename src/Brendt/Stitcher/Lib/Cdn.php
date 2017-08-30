612cf3e2225a46b2ef185f3ff06e5503fd48dfb9
BrenDt <brent@stitcher.io>
2017-08-30 07:17:06 +0200

    Add CDN support as a class

---
+++ src/Brendt/Stitcher/Lib/Cdn.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Brendt\Stitcher\Lib;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class Cdn
+{
+    private $browser;
+    private $files;
+    private $enableCache;
+
+    public function __construct(Browser $browser, array $files, bool $enableCache) {
+        $this->browser = $browser;
+        $this->files = $files;
+        $this->enableCache = $enableCache;
+    }
+
+    public function save() {
+        foreach ($this->files as $resource) {
+            $resource = trim($resource, '/');
+            $publicResourcePath = "{$this->browser->getPublicDir()}/{$resource}";
+
+            if ($this->enableCache && file_exists($publicResourcePath)) {
+                continue;
+            }
+
+            $sourceResourcePath = "{$this->browser->getSrcDir()}/{$resource}";
+            $this->copyCdnFiles($sourceResourcePath, $publicResourcePath);
+        }
+    }
+
+    private function copyCdnFiles(string $sourcePath, string $publicPath) {
+        $fs = new Filesystem();
+
+        if (is_dir($sourcePath)) {
+            $fs->mirror($sourcePath, $publicPath);
+        } else {
+            $fs->copy($sourcePath, $publicPath, true);
+        }
+    }
+}
