e5162e97c90d595aeb9bc11adc9f48d81c130582
Brent Roose <brent@spatie.be>
2017-11-11 14:15:34 +0100

    Add DevelopmentServer

--- src/Stitcher/Application/DevelopmentServer.php
+++ src/Stitcher/Application/DevelopmentServer.php
@@ -2,13 +2,34 @@
 
 namespace Stitcher\Application;
 
+use Stitcher\Command\PartialParse;
+
 class DevelopmentServer
 {
-    protected $html;
+    private $html;
 
-    public function __construct()
+    public function __construct(
+        PartialParse $partialParse,
+        string $rootDirectory,
+        string $uri = null
+    ) {
+        $uri = $uri ?? $_SERVER['SCRIPT_NAME'];
+
+        $partialParse->setFilter($uri);
+        $partialParse->execute();
+
+        $filename = ltrim($uri === '/' ? 'index.html' : "{$uri}.html", '/');
+
+        $this->html = @file_get_contents("{$rootDirectory}/{$filename}");
+    }
+
+    public static function make(
+        PartialParse $partialParse,
+        string $rootDirectory,
+        string $uri = null
+    ): DevelopmentServer
     {
-
+        return new self($partialParse, $rootDirectory, $uri);
     }
 
     public function run(): string
---
+++ tests/Stitcher/Application/DevelopmentServerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Stitcher\Application;
+
+use Stitcher\Command\PartialParse;
+use Stitcher\File;
+use Stitcher\Test\CreateStitcherFiles;
+use Stitcher\Test\CreateStitcherObjects;
+use Stitcher\Test\StitcherTest;
+
+class DevelopmentServerTest extends StitcherTest
+{
+    use CreateStitcherFiles;
+    use CreateStitcherObjects;
+
+    /** @var \Stitcher\Command\PartialParse */
+    private $command;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $configurationFile = File::path('config/site.yaml');
+
+        $this->createAllTemplates();
+        $this->createSiteConfiguration($configurationFile);
+        $this->createDataFile();
+        $this->createImageFiles();
+
+        $this->command = PartialParse::make(
+            File::path('public'),
+            $configurationFile,
+            $this->createPageParser(),
+            $this->createPageRenderer()
+        );
+    }
+
+    /** @test */
+    public function it_serves_static_html()
+    {
+        $server = DevelopmentServer::make($this->command, __DIR__ .'/../../../data/public', '/entries');
+
+        $html = $server->run();
+
+        $this->assertContains('<html>', $html);
+    }
+
+    /** @test */
+    public function it_serves_static_html_from_index()
+    {
+        $server = DevelopmentServer::make($this->command, __DIR__ .'/../../../data/public', '/');
+
+        $html = $server->run();
+
+        $this->assertContains('<html>', $html);
+    }
+}
