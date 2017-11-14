767fde45d2c931a7167dbc8b09c99f39cf1a1fd6
Brent Roose <brent@spatie.be>
2017-11-14 13:48:57 +0100

    Add FilesizeScaler

---
+++ src/Pageon/Html/Image/FilesizeScaler.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pageon\Html\Image;
+
+use Intervention\Image\Image as ScaleableImage;
+
+class FilesizeScaler implements Scaler
+{
+    private $stepModifier = 0.2;
+
+    public function __construct(float $stopModifier)
+    {
+        $this->stepModifier = $stopModifier;
+    }
+
+    public function getVariations(ScaleableImage $scaleableImage): array
+    {
+        $fileSize = $scaleableImage->filesize();
+        $width = $scaleableImage->width();
+
+        $ratio = $scaleableImage->height() / $width;
+        $area = $width * $width * $ratio;
+        $pixelPrice = $fileSize / $area;
+
+        $stepAmount = $fileSize * $this->stepModifier;
+
+        $variations = [];
+
+        do {
+            $newWidth = (int) floor(sqrt(($fileSize / $pixelPrice) / $ratio));
+
+            $variations[$newWidth] = (int) $newWidth * $ratio;
+
+            $fileSize -= $stepAmount;
+        } while ($fileSize > 0);
+
+        return $variations;
+    }
+}
