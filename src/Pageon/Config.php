6ce9705246666465ec6471f868f6a3893019ef4b
Brent Roose <brent@spatie.be>
2017-12-03 09:23:19 +0100

    Code cleanup

--- src/Pageon/Config.php
+++ src/Pageon/Config.php
@@ -5,15 +5,15 @@ namespace Pageon;
 use Dotenv\Dotenv;
 use Dotenv\Exception\InvalidPathException;
 use Illuminate\Support\Arr;
+use Iterator;
 use Stitcher\Exception\InvalidConfiguration;
 use Stitcher\File;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
 
 class Config
 {
     protected static $env;
-    protected static $loadedConfig = [];
+    protected static $loadedConfigfiguration = [];
 
     public static function init()
     {
@@ -25,31 +25,37 @@ class Config
             throw InvalidConfiguration::dotEnvNotFound(File::path());
         }
 
-        $configFiles = Finder::create()->files()->in(File::path('config'))->name('*.php');
+        $configurationFiles = Finder::create()->files()->in(File::path('config'))->name('*.php')->getIterator();
 
-        $unparsedConfig = [];
+        $loadedConfiguration = self::load($configurationFiles);
 
-        /** @var SplFileInfo $configFile */
-        foreach ($configFiles as $configFile) {
-            $fileConfig = require $configFile;
-
-            if (!is_array($fileConfig)) {
-                continue;
-            }
-
-            $unparsedConfig = array_merge($unparsedConfig, $fileConfig);
-        }
-
-        self::$loadedConfig = Arr::dot($unparsedConfig);
+        self::$loadedConfigfiguration = Arr::dot($loadedConfiguration);
     }
 
     public static function get(string $key)
     {
-        return self::$loadedConfig[$key] ?? null;
+        return self::$loadedConfigfiguration[$key] ?? null;
     }
 
     public static function all(): array
     {
-        return self::$loadedConfig;
+        return self::$loadedConfigfiguration;
+    }
+
+    protected static function load(Iterator $configurationFiles): array
+    {
+        $loadedConfiguration = [];
+
+        foreach ($configurationFiles as $configurationFile) {
+            $loadedFileConfiguration = require $configurationFile;
+
+            if (!is_array($loadedFileConfiguration)) {
+                continue;
+            }
+
+            $loadedConfiguration = array_merge($loadedConfiguration, $loadedFileConfiguration);
+        }
+
+        return $loadedConfiguration;
     }
 }
